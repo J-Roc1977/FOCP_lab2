da6fcb3c75461eb94d06d158e3d1b7060c5bc9d2
Bartosz Smolarz <bartsmo430@student.polsl.pl>
2020-10-15 14:42:06 +0200

    more changes

--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -1,33 +1,71 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
+int read_int()
+{
+	std::cout << "Please introduce a number: " << std::endl;
+	int n;
+	std::cin >> n;
+
+	return n;
+}
 
 int main()
 {
+	std::cout << "How many numbers do you want?" << std::endl;
+	int total_numbers = read_int();
+
+	std::vector<int> values;
+	// {}
+	// {[1]}
+
+	for (int i = 0; i < total_numbers; i++)
+	{
+		int tmp = read_int(); //1
+		values.push_back(tmp);
+	}
+
+
+
 	std::ofstream file;
-	file.open("new_file.txt");
-	file << "First line!\n";
-	file << "Second line!\n";
-	file << "Third line!\n";
-	file << "Fourth line!\n";
+	file.open("values.txt");
+	for (int i = 0; i < total_numbers; i++)
+	{
+		file << values[i];
+	}
 	file.close();
+
+	std::vector<int> read_values;
 
 	std::ifstream new_file("new_file.txt");
 
 	if (new_file.is_open())
 	{
 		std::string line;
-		while(getline(new_file, line))
+
+		while (getline(new_file, line))
 		{
-			std::cout << line;
+			read_values.push_back(std::stoi(line));
 		}
 
 		new_file.close();
 
+			}
+			else {
+				std::cout << "There was a problem opening the file" << std::endl;
+			}
+
+	int sum = 0;
+
+	for (int i = 0; i < total_numbers; i++) {
+		sum += read_values[i];
 	}
-	else {
-		std::cout << "There was a problem opening the file" << std::endl;
-	}
+
+	float mean = sum / total_numbers;
+
+	std::cout << "Mean = " << mean << std::endl;
 
 	return 0;
 }
