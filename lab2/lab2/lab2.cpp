6f2c22419c472a29ef39d822cb009e446c3e448d
Bartosz Smolarz <bartsmo430@student.polsl.pl>
2020-10-15 12:13:28 +0200

    prime number

--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -1,4 +1,22 @@
-﻿int main()
+﻿#include <iostream>
+
+int main()
 {
+    std::cout << "Please introduce a number: " << std::endl;
+
+    int number;
+    std::cin >> number;
+
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            std::cout << "Your number is not prime" << std::endl;
+            return 0;
+        }
+    }
+
+    std::cout << "Your number is prime" << std::endl;
+
     return 0;
 }
