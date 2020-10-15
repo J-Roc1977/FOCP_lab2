f0bbf20ce592ebc9c2fcc7599ca82093515b2518
Bartosz Smolarz <bartsmo430@student.polsl.pl>
2020-10-15 13:20:20 +0200

    Fibonacci recursive

--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -1,23 +1,28 @@
 ﻿#include <iostream>
 
-int fibonacci_iterative(int index)
-{
+int fibonacci_iterative(int index) {
     int a = 0;
     int b = 1;
 
-    if (index == 0 || index == 1)
-    {
+    if (index < 2) {
         return index;
     }
 
-    for (int i = 2; i < index; i++)
-    {
+    for (int i = 1; i < index; i++) {
         int tmp = a + b;
         a = b;
         b = tmp;
     }
 
-    return b;
+    return  b;
+}
+
+int fibonacci_recursive(int index) {
+    if (index < 2) {
+        return index;
+    }
+
+    return fibonacci_recursive(index - 2) + fibonacci_recursive(index - 1);
 }
 
 int read_int() {
@@ -32,9 +37,13 @@ int read_int() {
 int main() {
     int index = read_int();
 
-    int result = fibonacci_iterative(index);
+    int result_iterative = fibonacci_iterative(index);
 
-    std::cout << "The result is: " << result << std::endl;
+    std::cout << "The result with iteration is: " << result_iterative << std::endl;
+
+    int result_recursive = fibonacci_recursive(index);
+
+    std::cout << "The result with recursion is: " << result_recursive << std::endl;
 
     return 0;
 }
